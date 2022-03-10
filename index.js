691e5f47139c38644c1e914c7901c2990e815d19
Schuyler <skym1n0r91@gmail.com>
2022-03-10 11:58:18 -0600

    ude env.port

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 9000;
+
 const express = require("express");
 
 const server = express();
@@ -5,8 +9,9 @@ server.get("/", (req, res) => {
   res.end("<h1>hello world!</h1>");
 });
 
-server.listen(9000, () => {
-  console.log("server is on port 9000!");
+server.listen(PORT, () => {
+  console.log(process.env.MY_ENV);
+  console.log(`server is on port ${PORT}!, user is ${process.env.USERNAME}`);
 });
 
 // npx kill port 9000
